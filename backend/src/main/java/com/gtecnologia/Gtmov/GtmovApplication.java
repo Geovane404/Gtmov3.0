2cefbdf2c633bdac5aaf3b9c6be743e716657c7d
Geovane <geovanecalheiro@gmail.com>
2023-12-11 10:29:06 -0300

    projected created

---
+++ backend/src/main/java/com/gtecnologia/Gtmov/GtmovApplication.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.Gtmov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtmovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtmovApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gtecnologia/Gtmov/GtmovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.Gtmov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtmovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
